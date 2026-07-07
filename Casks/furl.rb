b7889a290ef21a023cb9d4cb44e8115d0d75fa84
julian <git@julianbaker.design>
2026-07-07 11:57:50 -0500

    furl 1.0.0

--- Casks/furl.rb
+++ Casks/furl.rb
@@ -1,6 +1,6 @@
 cask "furl" do
   version "1.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000" # updated by Furl's release pipeline
+  sha256 "7156452ea4f9d52b6acb454ad20f467696c3fe1d29575f24a8d3f78a12058c5a" # updated by Furl's release pipeline
 
   url "https://github.com/julianbaker/Furl/releases/download/v#{version}/Furl-#{version}.zip"
   name "Furl"
